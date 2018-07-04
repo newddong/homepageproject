7d2ed765be0f000036a62c623bd3881be3b15c04
Administrator <Administrator@sinchon-e-PC>
2018-07-04 18:39:08 +0900

    screen4 making

--- webProject/WebContent/js/project.js
+++ webProject/WebContent/js/project.js
@@ -7,11 +7,25 @@ $(function(){
 	
 	$(window).on("load resize",function(){
 		var height = window.innerHeight;
+		var width = window.innerWidth;
 		$(".screen1").height(height);
 		$(".screen2").height(height);
 		$(".screen3").height(height);
 		$(".screen4").height(height);
-	});
+		$(".content").height(height);
+		
+	});//load resize end
+	
+	
+	$(".children").children(0).on("mouseover",function(){
+		
+		$(".children").children(0).css({
+			background:url("../img/children_01_on.png") no-repeat;
+		});
+		
+		
+		
+	})//mouseover event end
 	
 	
 	
@@ -19,10 +33,7 @@ $(function(){
 	
 	
 	
-	
-	
-	
-});
+});//javascript end
 
 
 
