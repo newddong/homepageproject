da5f7d4fd0272889b6d49f0dfc0e7b65b7f1b65f
Administrator <Administrator@sinchon-e-PC>
2018-07-04 16:49:37 +0900

    layout working

--- webProject/WebContent/js/project.js
+++ webProject/WebContent/js/project.js
@@ -5,11 +5,13 @@
 
 $(function(){
 	
-//	$(window).resize(function(){
-//		var height = window.innerHeight;
-//		$(".screen1").height(height);
-//		$(".screen2").height(height);
-//	});
+	$(window).on("load resize",function(){
+		var height = window.innerHeight;
+		$(".screen1").height(height);
+		$(".screen2").height(height);
+		$(".screen3").height(height);
+		$(".screen4").height(height);
+	});
 	
 	
 	
