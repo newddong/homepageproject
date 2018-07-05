896012f1f5e236c62375cb80a5ed8283cbf350e7
newdd <newdd@DESKTOP-CLJ9RM4>
2018-07-05 13:45:03 +0900

    home work2

--- webProject/WebContent/js/project.js
+++ webProject/WebContent/js/project.js
@@ -8,11 +8,18 @@ $(function() {
 	$(window).on("load resize", function() {
 		var height = window.innerHeight;
 		var width = window.innerWidth;
-		$(".screen1").height(height);
-		$(".screen2").height(height);
-		$(".screen3").height(height);
-		$(".screen4").height(height);
-		$(".content").height(height);
+//		$(".screen1").height(height);
+//		$(".screen2").height(height);
+//		$(".screen3").height(height);
+//		$(".screen4").height(height);
+//		$(".screen5").height(height);
+//		$(".screen6").height(height);
+//		$(".screen7").height(height);
+//		$(".screen8").height(height);
+//		$(".screen9").height(height);
+//		$(".screen10").height(height);
+//		$(".screen11").height(height);
+		$(".main").height(height);
 
 	}); //load resize end
 	
